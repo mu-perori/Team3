b0134c29fd8a74b41117b6e466c28bd1b1c5ff6c
Ando Taichi <70427875+Taichi-ando@users.noreply.github.com>
2022-05-29 18:39:04 +0900

    create jpg & update main.py
    写真を追加して、main.pyを更新
    データベースも更新

--- app/main.py
+++ app/main.py
@@ -33,8 +33,8 @@ db_path = (
 images_path = pathlib.Path(os.path.dirname(__file__)).parent.resolve() / "images"
 
 
-class Want_keyword(BaseModel):
-    keyword: str
+# class Want_keyword(BaseModel):
+#     keyword: str
 
 
 class Want_input(BaseModel):
@@ -48,8 +48,8 @@ class Want_input(BaseModel):
     item_id: int = None
 
 
-class Want_item(BaseModel):
-    want_id: int
+# class Want_item(BaseModel):
+#     want_id: int
 
 
 @app.get("/")
@@ -78,93 +78,115 @@ def add_want(want_input: Want_input):
     )
     con.commit()
     con.close()
-    return {"message": "登録しました！", "path": db_path}
+    return {"message": "登録しました！"}
 
 
-#探してますコーナーのトップ画面
+# 探してますコーナーのトップ画面
 @app.get("/want")
 def pickup():
     con = sqlite3.connect(db_path, check_same_thread=False)
     cur = con.cursor()
     # カテゴリは今回は４のみの用意
-    categories = ["カメラ","本","服","文房具"]
-    tmp_dict = dict()
+    categories = ["カメラ", "本", "レディース", "スポーツ"]
+    tmp_dict = []
 
     for i in range(4):
-        cid = "category{}".format(i+1)
-
+        cid = "category{}".format(i + 1)
 
         cname = categories[i]
-        items_list = cur.execute("select item_id,item_name,item_price from wants where category = :c limit 5",{"c":cname}).fetchall()
-        for j in range(5):
-            item_id,item_name,item_price = *items_list[j]
+        items_list = cur.execute(
+            "select item_id,item_name,budget from wants where category = :c limit 5",
+            {"c": cname},
+        ).fetchall()
+        print(items_list)
+        for j in range(len(items_list)):
+            item_id, item_name, budget = items_list[j]
 
-            item_image_name = cur.execute("select item_image_name from items where item_id = :id",{"id":item_id}).fetchone()
+            item_image_name = cur.execute(
+                "select item_image_name from items where item_id = :id", {"id": item_id}
+            ).fetchone()[0]
 
-            items_list[j] = {"item_id":item_id ,"item_name":item_name,"item_price":item_price,"item_image_name":item_image_name}
+            items_list[j] = {
+                "item_id": item_id,
+                "item_name": item_name,
+                "budget": budget,
+                "item_image_name": item_image_name,
+            }
 
-        avg_bud = int(cur.execute("select avg(item_price) from wants where category = :c",{"c":cname}).fetchone())
+        avg_bud = cur.execute(
+            "select avg(budget) from wants where category = :c", {"c": cname}
+        ).fetchone()
+        print(avg_bud[0])
+        print(type(avg_bud[0]))
 
-        tmp_dict[cid] = {"category":cname,
-                        "avg_budget":avg_bud,
-                        "items":items_list}
-    
+        tmp_dict.append(
+            {
+                "category": cname,
+                "avg_budget": round(avg_bud[0]),
+                "items": items_list,
+            }
+        )
+
     con.close()
-    return json.dumps(tmp_dict)
+    return tmp_dict
 
-  
+
 @app.get("/want/results")
-def search(want: Want_keyword):
+def search(keyword: str):
     con = sqlite3.connect(db_path, check_same_thread=False)
     cur = con.cursor()
-    tmp_set = set()
-    keyword = want.keyword
-    items_list = cur.execute("select item_id,item_name,item_price from wants where category = ?  or item_name = ? limit 20",(keyword,keyword)).fetchall()
-
+    tmp_set = []
+    items_list = cur.execute(
+        "select item_id,item_name,budget from wants where category = ?  or item_name = ? limit 20",
+        (keyword, keyword),
+    ).fetchall()
 
     for item in items_list:
-        item_id,item_name,item_price = *item
-        item_image_name = cur.execute("select item_image_name from items where item_id = :id",{"id":item_id}).fetchone()
+        item_id, item_name, budget = item
+        item_image_name = cur.execute(
+            "select item_image_name from items where item_id = :id", {"id": item_id}
+        ).fetchone()
 
-        tmp_set.add(json.dumps( {"item_id":item_id ,"item_name":item_name,"item_price":item_price,"item_image_name":item_image_name}))
+        tmp_set.append(
+            {
+                "item_id": item_id,
+                "item_name": item_name,
+                "budget": budget,
+                "item_image_name": item_image_name[0],
+            }
+        )
 
     con.close()
-    return json.dumps(tmp_set)
+    return tmp_set
 
 
 # 探されているアイテムの詳細画面
 @app.get("/want/item")
-def view_item(want_item: Want_item):
+def view_item(want_id: str):
     con = sqlite3.connect(db_path, check_same_thread=False)
     cur = con.cursor()
     cur.execute(
-        "SELECT wants.item_id FROM wants WHERE wants.item_id = ?", (want_item.want_id,)
+        "SELECT wants.item_id FROM wants WHERE wants.wants_id = ?", (int(want_id),)
     )
     res = cur.fetchall()
-    if res:
+    if res[0][0]:
         cur.execute(
             "SELECT items.item_image_name FROM items WHERE items.item_id = ?",
-            (res,),
+            (int(res[0][0]),),
         )
-        image_name = cur.fetchall()
+        image_name = cur.fetchall()[0][0]
     else:
         image_name = "no_image"
     cur.execute(
-        "SELECT wants.item_name, wants.category, wants.item_discription, wants.item_status FROM wants WHERE wants.wants_id = ?",
-        (want_item.want_id,),
+        "SELECT wants.item_name, wants.category, wants.item_discription, wants.item_status, wants.budget FROM wants WHERE wants.wants_id = ?",
+        (int(want_id),),
     )
     items = cur.fetchall()
-    print(items)
-    cur.execute(
-        "SELECT wants.budget FROM wants WHERE wants.item_name = ?", (items[0][0],)
-    )
     budgets = cur.fetchall()
     con.close()
-    avg_budget = sum([i[0] for i in budgets]) / len(budgets)
-    print(budgets)
     return {
         "item_name": items[0][0],
-        "avg_budget": int(avg_budget),
+        "budget": items[0][4],
         "category": items[0][1],
         "item_status": items[0][3],
         "item_discription": items[0][2],
